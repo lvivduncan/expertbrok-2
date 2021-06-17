1db758bda7845ffc76ea40bec92c5e609b1791f3
lvivduncan <lvivduncan@gmail.com>
2021-06-17 14:16:35 +0300

    partners

--- js/init.js
+++ js/init.js
@@ -49,4 +49,48 @@ if(localStorage.lang == 1){
 $('#nav-button').on('click',function(){
     $(this).toggleClass('active');
     $('#menu').toggleClass('active');
-})
+});
+
+
+
+
+// test partners
+$(document).ready(function() {
+    if ( $(window).width() < 854 ) {
+        startCarousel();
+    } else {
+        $('.partners .owl-carousel').addClass('off');
+    }
+});
+
+$(window).resize(function() {
+    if ( $(window).width() < 854 ) {
+        startCarousel();
+    } else {
+        stopCarousel();
+    }
+});
+
+function startCarousel(){
+    $(".partners .owl-carousel").owlCarousel({
+        navigation : true, // Show next and prev buttons
+        slideSpeed : 500,
+        margin:10,
+        paginationSpeed : 400,
+        autoplay:true,
+        items : 1,
+        itemsDesktop : false,
+        itemsDesktopSmall : false,
+        itemsTablet: false,
+        itemsMobile : false,
+        loop:true,
+        nav:true,
+        navText: ["<i class='fa fa-angle-left' aria-hidden='true'></i>","<i class='fa fa-angle-right' aria-hidden='true'></i>"]
+    });
+}
+
+function stopCarousel() {
+    var owl = $('.partners .owl-carousel');
+    owl.trigger('destroy.owl.carousel');
+    owl.addClass('off');
+}
